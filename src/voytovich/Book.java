673666695c4a9ea7291afd75c259ea5280c6dfe2
svintuskbd <swintuskbd@gmail.com>
2017-11-28 21:33:15 +0200

    add to_string, equals, hashcode

--- src/voytovich/Book.java
+++ src/voytovich/Book.java
@@ -25,7 +25,36 @@ public class Book {
         return authors;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (years != book.years) return false;
+        if (title != null ? !title.equals(book.title) : book.title != null) return false;
+        return authors != null ? authors.equals(book.authors) : book.authors == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (authors != null ? authors.hashCode() : 0);
+        result = 31 * result + years;
+        return result;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", authors=" + authors +
+                ", years=" + years +
+                '}';
     }
 }
